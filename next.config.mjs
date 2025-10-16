51844049bdbd940ae58b6fb5db844bfa8a694c3c
programmer9176 <myeducation9176@gmail.com>
2025-10-17 00:09:25 +0500

    added redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "cdn.mdsbrand.com",
-                pathname: "/**",
-            },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.mdsbrand.com",
+        pathname: "/**",
+      },
+    ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'hafsa-health-blogger.vercel.app',
+          },
         ],
-    },
+        destination: 'https://www.thehealthsheet.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
