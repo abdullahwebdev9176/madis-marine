057cf2daffacbb90d46be5bd177ed5b43cce3cb1
programmer9176 <myeducation9176@gmail.com>
2025-10-21 02:01:52 +0500

    updated faq page

--- app/FaqTheme2/page.jsx
+++ app/FaqTheme2/page.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+
+'use client'
+
+import React, { useState } from 'react'
 import '@/styles/pages/FaqTheme2.css'
+import { faqData } from '@/Data/faqData'
 
 const FaqTheme2 = () => {
+
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleAccordion = (index) => {
+
+    setActiveIndex(activeIndex === index ? null : index)
+
+  }
+
   return (
     <>
       <section className="staf-sec">
@@ -13,246 +26,27 @@ const FaqTheme2 = () => {
             <div className="col-md-10 col-xl-7 m-auto">
               <div>
                 <div className="custom-accordion my-3 my-md-5 pb-md-5">
-                  <div className="accordion-card">
-                    <div className="custom-accordion-header">
-                      <h1 role="heading">SALES</h1>
-                      <span className="fa fa-minus" />
-                    </div>
-                    <div className="custom-accordion-body content-show px-2 px-sm-4" style={{ display: 'block' }}>
-                      <div className="my-3 my-md-5">
-                        <h4 role="heading"><span>Q.</span>
-                          Q. Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
+                  {faqData.map((item, index) => {
+                    return (
+                      <div className="accordion-card" key={index}>
+                        <div className="custom-accordion-header" onClick={() => toggleAccordion(index)}>
+                          <h1 role="heading">{item.question}</h1>
+                          <span className={`fa ${activeIndex === index ? "fa-minus" : "fa-plus"}`} />
+                        </div>
+                        {activeIndex === index && (
+                          <div className="custom-accordion-body content-show px-2 px-sm-4">
+                            <div
+                              className="my-3"
+                              dangerouslySetInnerHTML={{ __html: item.answer }}
+                            />
+                          </div>
+                        )}
+
                       </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span>Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="accordion-card">
-                    <div className="custom-accordion-header">
-                      <h1 role="heading">Service</h1>
-                      <span className="fa fa-minus" />
-                    </div>
-                    <div className="custom-accordion-body px-2 px-sm-4">
-                      <div className="my-3 my-md-5">
-                        <h4 role="heading"><span>Q.</span>
-                          Q. Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span>Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="accordion-card">
-                    <div className="custom-accordion-header">
-                      <h1 role="heading">Company</h1>
-                      <span className="fa fa-minus" />
-                    </div>
-                    <div className="custom-accordion-body px-2 px-sm-4">
-                      <div className="my-3 my-md-5">
-                        <h4 role="heading"><span>Q.</span>
-                          Q. Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span> Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                      <hr className="my-4 my-md-5" />
-                      <div className="my-4 my-md-5">
-                        <h4><span>Q.</span> QUESTION</h4>
-                        <p className="mt-3"><span>A.</span>Lorem ipsum dolor sit amet, consectetur
-                          adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                          enim
-                          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea
-                          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur.</p>
-                      </div>
-                    </div>
-                  </div>
+                    )
+                  })}
+
+
                 </div>
               </div>
             </div>
